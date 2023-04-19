cdebf1b019ad339cf8351e48d730927bc91a557a
kriyip <kyip719@gmail.com>
2023-04-19 13:21:21 -0400

    refined ask_question function

--- GPTerminal.sh
+++ GPTerminal.sh
@@ -7,7 +7,7 @@ if [[ -z "$OPENAI_API_KEY" ]]; then
         exit 1
 fi
 
-echo $OPENAI_API_KEY
+# echo $OPENAI_API_KEY
 # Parse command line arguments (check right args given)
 if [ $# -eq 0 ]; then
 	echo "Please enter a question"
@@ -18,21 +18,19 @@ fi
 # note that response is JSON-formatted result from making request with curl
 # next steps: output response in a more readable form
 function ask_question {
-        echo "Asking question: $prompt"
+        echo "First parameter: $1"
         
         response=$(curl https://api.openai.com/v1/chat/completions \
                 -H 'Content-Type: application/json' \
                 -H "Authorization: Bearer $OPENAI_API_KEY" \
                 -d '{
                 "model": "gpt-3.5-turbo",
-                "messages": [{"role": "user", "content": "'"$prompt"'"}],
+                "messages": [{"role": "user", "content": "'"$1"'"}],
                 "temperature": 0.7
                 }')
-        echo $response
-        echo $response | python -c "import sys, json; print(json.load(sys.stdin))"
+        # echo $response
+        echo $response | python -c "import sys, json; print(json.load(sys.stdin)['choices'][0]['message']['content'])"
 }
 
-# answer=$(ask_question)
-prompt=$1
-ask_question
+ask_question "$1"
 # echo $1
