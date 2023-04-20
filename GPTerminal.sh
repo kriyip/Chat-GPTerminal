1a1a315c7054176e93c5efef5c738eb7fc3fc65f
kriyip <kyip719@gmail.com>
2023-04-20 00:55:08 -0400

    started function for maintaining chat context

--- GPTerminal.sh
+++ GPTerminal.sh
@@ -7,9 +7,9 @@ EXPLAIN_CODE_INIT_PROMPT="You are summarizing a code snippet in natural language
 TEMPERATURE="0.7"
 
 # 1 for context, 0 for no context
-CONTEXT=1
+HAS_CONTEXT=1
 
-CONTEXT_FILE_PATH="~/GPTerminal_History/chat_1"
+CONTEXT_FILE_PATH=""
 MODEL="gpt-3.5-turbo"
 
 # 1 for verbose, 0 for not verbose
@@ -17,20 +17,41 @@ VERBOSE=0
 
 USER_INIT_PROMPT=""
 
-# check if OpenAI API key is set as environment variable
-if [[ -z "$OPENAI_API_KEY" ]]; then
-        echo "Please set your OpenAI API key as the environment variable OPENAI_API_KEY by running: export OPENAI_API_KEY=..."
-        echo "You can create an API key at https://beta.openai.com/account/api-keys"
-        exit 1
-fi
-
-function init {
+function init_context_file {
         # check if context file exists
-        if [ ! -f "$CONTEXT_FILE_PATH" ]; then
-                touch "$CONTEXT_FILE_PATH"
-                chmod 700 "$CONTEXT_FILE_PATH"
+        if [ ! -d "~/.GPTerminal" ]; then
+                mkdir "~/.GPTerminal"
+                mkdir "~/.GPTerminal/History"
         fi
+        if [ -n "$CONTEXT_FILE_PATH" ]; then
+                timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+                CONTEXT_FILE_PATH="~/.GPTerminal/History/$timestamp"
+        fi
+        touch "$CONTEXT_FILE_PATH"
+        chmod 700 "$CONTEXT_FILE_PATH"
 }
+
+# called before each ask_question request
+function get_context {
+        # get context from context file
+        context=$(cat "$CONTEXT_FILE_PATH")
+        # may need to grep based on certain keywords? or is there some way I could summarize a text file?
+        # first, will just make it work with basic context file and then try to make longer conversations last.
+        # will drop the previous context files
+        echo $context
+}
+
+build_chat_context() {
+	chat_context="$1"
+	escaped_prompt="$2"
+	if [ -z "$chat_context" ]; then
+		chat_context="$CHAT_INIT_PROMPT\nQ: $escaped_prompt"
+	else
+		chat_context="$chat_context\nQ: $escaped_prompt"
+	fi
+	request_prompt="${chat_context//$'\n'/\\n}"
+}
+
 
 # use curl to send question data to OpenAI server and get the response
 # note that response is JSON-formatted result from making request with curl
@@ -50,7 +71,15 @@ function ask_question {
         echo $response | python -c "import sys, json; print(json.load(sys.stdin)['choices'][0]['message']['content'])"
 }
 
-ask_question "$1"
+
+
+
+# check if OpenAI API key is set as environment variable
+if [[ -z "$OPENAI_API_KEY" ]]; then
+        echo "Please set your OpenAI API key as the environment variable OPENAI_API_KEY by running: export OPENAI_API_KEY=..."
+        echo "You can create an API key at https://beta.openai.com/account/api-keys"
+        exit 1
+fi
 
 # parse flags/cli
 while [[ $# -gt 0 ]]; do
@@ -92,11 +121,13 @@ while [[ $# -gt 0 ]]; do
                         shift
                         shift
                         ;;
-                -c | --context )
-                        if [ "$2" = "on" ]; then
-                                CONTEXT=1
+                --context )
+                        if [ "$2" = "drop" ]; then
+                                HAS_CONTEXT=1
+                        elif [ "$2" = "summarize"]; then
+                                HAS_CONTEXT=2
                         elif [ "$2" = "off" ]; then
-                                CONTEXT=0
+                                HAS_CONTEXT=0
                         else
                                 echo "Invalid context: $2"
                                 exit 1
@@ -104,10 +135,46 @@ while [[ $# -gt 0 ]]; do
                         shift
                         shift
                         ;;
-                -q | --question )
-                        ask_question "$2"
+                -v | --verbose )
+                        if [ "$2" = "on" ]; then
+                                VERBOSE=1
+                        elif [ "$2" = "off" ]; then
+                                VERBOSE=0
+                        else
+                                echo "Invalid verbose: $2"
+                                exit 1
+                        fi
+                        shift
+                        shift
                         ;;
-                *)
+                -c | --continue-chat )
+                        if [ -n $2 ]; then
+                                if [ -e "~/.GPTerminal/History/$2" ]; then
+                                        CONTEXT_FILE_PATH="~/.GPTerminal/History/$2"
+                                else
+                                        echo "Chat $2 does not exist"
+                                        exit 1
+                                fi
+                        else
+                                echo "Invalid chat name: $2"
+                                exit 1
+                        fi
+                        shift
+                        shift
+                        ;;
+                --history ) # takes in chatname parameter
+                        if [ -n "~/.GPTerminal/History/$2" ]; then
+                                less "~/.GPTerminal/History/$2"
+                                exit 1
+                        else
+                                echo "No such chat exists"
+                        fi
+                        ;;
+                -q | --question ) # ask question and exit. context/history will not be saved
+                        ask_question "$2"
+                        exit 1
+                        ;;
+                * )
                         echo "Invalid option: $1"
                         exit 1
                         ;;
@@ -118,11 +185,7 @@ echo "User init prompt: $USER_INIT_PROMPT"
 echo "Model: $MODEL"
 echo "Temperature: $TEMPERATURE"
 
-# Parse command line arguments (check right args given)
-if [ $# -eq 0 ]; then
-	echo "Please enter a question"
-	exit 1
-fi
+init_context_file
 
 # type exit to exit
 while true; do
